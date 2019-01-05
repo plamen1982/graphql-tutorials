f167b118a46f0a936dabd886b5633a724f9f7d1d
“[plamen1982]” <plamen.hristov@ymail.com>
2019-01-05 23:45:39 +0200

    Added dummy data for the basic schema

--- full-stack-graph-ql-project/server/schema/schema.js
+++ full-stack-graph-ql-project/server/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require('qraphql');
-
+const _ = require('lodash');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+//dummy data
+const books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
+]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -19,6 +26,8 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString } }, // book(id: '1') when we call our qeury - id is coming from the args.
             resolve(parent, args) {
                 //code to get data from db / other source
+                // return books.find((book) => args.id === book.id)
+                return _.find(books, { id: args.id })
             }
         }
     }
