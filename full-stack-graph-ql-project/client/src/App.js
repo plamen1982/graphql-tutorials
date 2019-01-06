38aa5d42714440cd7801902f80ec3bf0bdbcb1a3
“[plamen1982]” <plamen.hristov@ymail.com>
2019-01-06 12:12:10 +0200

    Created simple component BookList

--- full-stack-graph-ql-project/client/src/App.js
+++ full-stack-graph-ql-project/client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+
+//components
+import BookList from './components/BookList';
 
 class App extends Component {
   render() {
     return (
       <div id="main">
         <h1>Ninja's Reading List</h1>
+        <BookList />
       </div>
     );
   }
