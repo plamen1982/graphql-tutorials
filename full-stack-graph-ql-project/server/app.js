5e5a3fb5e9a68ce0e274d1948d10d787b8eb8f04
“[plamen1982]” <plamen.hristov@ymail.com>
2019-01-06 13:04:00 +0200

    Allowing CORS in the server

--- full-stack-graph-ql-project/server/app.js
+++ full-stack-graph-ql-project/server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+//allow cross-origin requests - CORS
+app.use(cors());
 
 //connect to mlab database
 mongoose.connect('mongodb://pax:graphql-ninja1@ds149754.mlab.com:49754/gql-ninja');
