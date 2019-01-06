0c3e84f1ea5e8d110936a5241e9cdc1c4b8d9bb5
“[plamen1982]” <plamen.hristov@ymail.com>
2019-01-06 14:49:43 +0200

    compose(connect) more that one graphql guery to one component

--- full-stack-graph-ql-project/client/src/components/AddBook.js
+++ full-stack-graph-ql-project/client/src/components/AddBook.js
@@ -1,64 +1,77 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo'; // graphql is the tool that helping us to bind the data from the query with the component
-import { getAuthorsQuery } from '../queries/queries';
+import React, { Component } from "react";
+import { graphql, compose } from "react-apollo"; // graphql is the tool that helping us to bind the data from the query with the component
+import { getAuthorsQuery, addBookMutation } from "../queries/queries";
 
 class AddBook extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            genre: '',
-            authorId: '',
-        }
-    }
-    renderAuthors() {
-        const { data: { authors } } = this.props;
-        const { data: { loading } } = this.props;
-        if(loading) {
-            return <option>Loading Authors...</option>
-        }
-
-        return (
-            authors.map(author => {
-                return <option key={author.id} value={author.id}>{author.name}</option>
-            })
-        );
-        
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      genre: "",
+      authorId: ""
+    };
+  }
+  renderAuthors() {
+    const {
+      getAuthorsQuery: { authors }
+    } = this.props;
+    const {
+      getAuthorsQuery: { loading }
+    } = this.props;
+    if (loading) {
+      return <option>Loading Authors...</option>;
     }
 
-    submitForm = (e) => {
-        e.preventDefault();
-        console.log(this.state);
-    }
+    return authors.map(author => {
+      return (
+        <option key={author.id} value={author.id}>
+          {author.name}
+        </option>
+      );
+    });
+  }
+
+  submitForm = e => {
+    e.preventDefault();
+    this.props.addBookMutation();
+  };
 
   render() {
-
     return (
-        <form id="add-book" onSubmit={this.submitForm}>
+      <form id="add-book" onSubmit={this.submitForm}>
         <div className="field">
-            <label>Book name:</label>
-            <input type="text" onChange={ e => this.setState({ name: e.target.value })}/>
+          <label>Book name:</label>
+          <input
+            type="text"
+            onChange={e => this.setState({ name: e.target.value })}
+          />
         </div>
         <div className="field">
-            <label>Genre:</label>
-            <input type="text" onChange={ e => this.setState({ genre: e.target.value })}/>
+          <label>Genre:</label>
+          <input
+            type="text"
+            onChange={e => this.setState({ genre: e.target.value })}
+          />
         </div>
         <div className="field">
-            <label>Author:</label>
-            <select onChange={ e => this.setState({ authorId: e.target.value })}>
-                <option>Select author</option>
-                {this.renderAuthors()}
-            </select>
+          <label>Author:</label>
+          <select onChange={e => this.setState({ authorId: e.target.value })}>
+            <option>Select author</option>
+            {this.renderAuthors()}
+          </select>
         </div>
         <button>+</button>
-
-    </form>
+      </form>
     );
   }
 }
 
-export default graphql(getAuthorsQuery)(AddBook); //when component renders the data is requested with the query and the binding with the 
-                                                //component and the information from the query is stored in the this.props
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }), // getAuthorsQuery is going to be attached to this.props and inside is going to be the authors array
+  graphql(addBookMutation, { name: "addBookMutation" }) // addBookMutation is going to be attached to this.props
+)(AddBook); //when component renders the data is requested with the query and the binding with the
+//component and the information from the query is stored in the this.props
+//compose is used for binding several queries to one component
 
 //1. constructing the query
 //2. binding the query result with the component
--- full-stack-graph-ql-project/client/src/queries/queries.js
+++ full-stack-graph-ql-project/client/src/queries/queries.js
@@ -1,21 +1,30 @@
-import { gql } from 'apollo-boost';  // gql is the tool that helping us to construct the query, since GraphQL is not a javascript, but rather diff language
+import { gql } from "apollo-boost"; // gql is the tool that helping us to construct the query, since GraphQL is not a javascript, but rather diff language
 
 const getAuthorsQuery = gql`
-    {
-        authors {
-            name
-            id
-        }
+  {
+    authors {
+      name
+      id
     }
+  }
 `;
 
 const getBooksQuery = gql`
-    {
-        books {
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const addBookMutation = gql`
+    mutation {
+        addBook(name: "", genre: "", authorId: "") {
             name
             id
         }
     }
 `;
 
-export { getAuthorsQuery, getBooksQuery };
+export { getAuthorsQuery, getBooksQuery, addBookMutation };
