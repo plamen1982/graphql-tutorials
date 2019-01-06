a571cb7c6b9ba1d9ad45215692091da1839f58f1
“[plamen1982]” <plamen.hristov@ymail.com>
2019-01-06 12:25:30 +0200

    Integrated Apollo into our React.js client app

--- full-stack-graph-ql-project/client/src/App.js
+++ full-stack-graph-ql-project/client/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'; //binding react with apollo and inject data from the server into our app
 //components
 import BookList from './components/BookList';
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql';
+})
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Ninja's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
